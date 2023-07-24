d03f082f308d63a9fc2fbea59f0bf6bd906119c8
natalia1310 <101365749+natalia1310@users.noreply.github.com>
2023-07-24 16:56:14 +0300

    Add files via upload

---
+++ tema2.cpp
@@ -0,0 +1,373 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <shader.h>
+#include <camera.h>
+
+#include <iostream>
+
+//functie de tratare a dimensiunii ecranului
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+//functie de tratare a evenimentelor de la mouse
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+//functia de tratare a evenimentelor de la scroll
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+void processInput(GLFWwindow* window);
+
+//functie de tratare a evenimentelor de la tastatura
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.3f, 1.7f, 15.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// lighting
+glm::vec3 lightPos(1.0f, 0.5f, 4.0f);
+float power = 5.0f;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS) {
+		power += 0.1f;
+		std::cout << "Power: " << power << std::endl;
+	}
+
+	if (key == GLFW_KEY_MINUS && action == GLFW_PRESS) {
+		power -= 0.1f;
+		std::cout << "Power: " << power << std::endl;
+	}
+}
+
+int main()
+{
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	// glfw window creation
+	// --------------------
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetKeyCallback(window, key_callback);
+
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// glad: load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// configure global opengl state
+	glEnable(GL_DEPTH_TEST);
+
+	// build and compile our shader zprogram
+	// ------------------------------------
+	Shader lightingShader("C:/Users/GA/Documents/Visual Studio 2022/Projects/Tema2/basic_lighting.vs", "C:/Users/GA/Documents/Visual Studio 2022/Projects/Tema2/basic_lighting.fs");
+	Shader lampShader("C:/Users/GA/Documents/Visual Studio 2022/Projects/Tema2/lamp.vs", "C:/Users/GA/Documents/Visual Studio 2022/Projects/Tema2/lamp.fs");
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		
+		0.5f, -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f,  0.5f, 1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f,  0.5f, 1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f,  0.5f, 1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f, -0.5f, 1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+
+	glm::mat4 modelS, modelP, modelL, view, projection;
+
+	// first, configure the cube's VAO (and VBO)
+	unsigned int VBO, cubeVAO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(cubeVAO);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	// second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// note that we update the lamp's position attribute's stride to reflect the updated buffer data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window))
+	{
+		// per-frame time logic
+		// --------------------
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// input
+		// -----
+		processInput(window);
+
+		//float radius = 4.0f;
+		//float x = sin(glfwGetTime()) * radius;
+		//float z = cos(glfwGetTime()) * radius;
+		//lightPos = glm::vec3(x, 0.5f, z);
+
+		// render
+		// ------curatarea buffer-ului de culoare si adancime
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// be sure to activate shader when setting uniforms/drawing objects
+		lightingShader.use();
+		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("viewPos", camera.Position);
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		view = camera.GetViewMatrix();
+
+
+		//lightingShader.setMat4("projection", projection);
+		//lightingShader.setMat4("view", view);
+		 
+		float angleGlobal = glm::radians(20.0f) * currentFrame; //pentru a controla rotatia generala a intregii scene
+		float angleLocal = glm::radians(30.0f) * currentFrame;//pentru a controla rotația locală a unui obiect în jurul axei sale proprii
+		float angleLocalP = glm::radians(30.0f) * currentFrame;//pentru ca P are doua rotatii, folosim angleLocal pentru a controla rotatia in jurul axei sale
+		
+		// Matricea pentru rotația globală
+		glm::mat4 rotationGlobal = glm::rotate(glm::mat4(1.0f), angleGlobal, glm::vec3(0.0f, 1.0f, 0.0f));
+		// Matricea pentru rotația locală
+		glm::mat4 rotationLocal = glm::rotate(glm::mat4(1.0f), angleLocal, glm::vec3(0.0f, 1.0f, 0.0f));
+
+		// Matricea pentru rotația globală a cubului S
+		glm::mat4 rotationGlobalS = glm::rotate(glm::mat4(1.0f), angleGlobal, glm::vec3(0.0f, 1.0f, 0.0f));
+		// Matricea pentru rotația locală a cubului P
+		glm::mat4 rotationLocalP = glm::rotate(glm::mat4(1.0f), angleLocalP, glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+		// Desenarea cubului S
+		glm::mat4 modelS = glm::mat4(1.0f);
+		modelS = glm::translate(modelS, glm::vec3(0.0f, 0.0f, 0.0f));
+		modelS = glm::rotate(modelS, angleGlobal, glm::vec3(0.2f, 1.0f, 0.0f));
+		lightingShader.setMat4("model", modelS);
+		lightingShader.setMat4("view", view);
+		lightingShader.setMat4("projection", projection);
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// Desenarea cubului P
+		glm::mat4 modelP = glm::mat4(1.0f);
+		modelP = glm::translate(modelP, glm::vec3(4.0f, 0.0f, 0.0f));
+		modelP = rotationGlobalS * modelP; //matrice de transformare care combina rot.globala a scene(inclusiv a ob.S) cu rot. locala a obiectului P
+		modelP = rotationLocalP * modelP;//combina rotatia locala a obiectului P cu pozitia sa actuala in raport cu scena
+		modelP = glm::rotate(modelP, angleGlobal, glm::vec3(0.2f, 1.0f, 0.0f));
+		lightingShader.setMat4("model", modelP);
+		lightingShader.setMat4("view", view);
+		lightingShader.setMat4("projection", projection);
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		// Desenarea cubului L
+		glm::mat4 modelL = glm::mat4(1.0f);
+		modelL = glm::translate(modelL, glm::vec3(2.0f, 0.0f, 0.0f));
+		modelL = rotationLocal * modelL;
+		modelL = modelP * rotationGlobal * modelL;//combina transformarile individuale ale obiectelor P si L cu transformările globale ale întregii scene
+		lightingShader.setMat4("model", modelL);
+		lightingShader.setMat4("view", view);
+		lightingShader.setMat4("projection", projection);
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &VBO);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	//--------------------------
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(UP, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(DOWN, deltaTime);
+	}
+
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
